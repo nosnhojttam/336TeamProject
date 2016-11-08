6b4bfe5032b06c7c6fd852ec4c3e7c2b5fed1569
Bryan Rodriguez <funones110@gmail.com>
2016-11-07 16:44:30 -0800

    Progress so far.

--- index.php
+++ index.php
@@ -34,6 +34,69 @@
             echo "</tr>";
         }
         echo "</table>";
+        
+           if (isset($_GET['submit']))
+            {
+                $namedParameters = array();
+                
+                if (!empty($_GET['itemName']))
+                    {
+                   $sql = $sql . " AND itemName LIKE  :itemName";
+                   $namedParameters[':itemName'] = "%" . $_GET['itemName'] . "%";
+                    }
+                    if(isset($_GET['inStore']))
+                     {
+                    $sql = $sql . " AND availability = :availability";
+                    $namedParameters[':availability'] = "inStore";
+                    }
+                
+                 if(isset($_GET['online']))
+                    {
+                    $sql = $sql . " AND availability = :availability";
+                    $namedParameters[':availability'] = "online";
+                    }
+                
+                if(isset($_GET['mens']))
+                    {
+                    $sql = $sql . " AND deptId = :deptId";
+                    $namedParameters[':deptId'] = "1";
+                    }
+                
+                 if(isset($_GET['womens']))
+                    {
+                    $sql = $sql . " AND deptId = :deptId";
+                    $namedParameters[':deptId'] = "2";
+                    }
+                
+                if(isset($_GET['kids']))
+                    {
+                    $sql = $sql . " AND deptId = :deptId";
+                    $namedParameters[':deptId'] = "3";
+                    }
+            }
+        
+             if(isset($_GET['sort']))
+                {
+                    $sortN = $_GET['sort'];
+                    
+                    
+                    if($sortN = 'pricehigh')
+                        {
+                        $sql= $sql . 'ORDER BY price DESC';
+                        }
+                }
+                    
+                     if(isset($_GET['sort1']))
+                    {
+                    $sortN1 = $_GET['sort1'];
+                    }
+                    
+                    if ($sortN1='item')
+                    {
+                        $sql=$sql . 'ORDER BY itemName';
+                    }
+         
+    
     }
 ?>
 
@@ -50,6 +113,28 @@
     
         <form>
             <h1>put filtering/sorting here</h1>
+               <input type= "checkbox" name= "inStore" id ="inStore" value="inStore">
+             <label for="inStore" > In Store</label>
+             
+             <input type= "checkbox" name= "available" id ="available" value="available">
+             <label for="online" > Online</label>
+             
+             <input type= "checkbox" name= "kids" id ="kids" value="kids">
+             <label for="kids" > Kid's</label>
+             
+               <input type= "checkbox" name= "mens" id ="mens" value="mens">
+             <label for="mens" > Men's</label>
+             
+             <input type= "checkbox" name= "womens" id ="womens" value="womens">
+             <label for="womens" > Women's</label>
+             
+             <input type= "checkbox" name= "sort" id ="sort" value="sort">
+             <label for="sort" > Order by Price</label>
+             
+              <input type= "checkbox" name= "sort" id ="sort" value="sort">
+             <label for="sort1" > Order by Name</label>
+             
+             <input type="submit" name ="submit" value="Search"/></td>
         </form>
 
         <br /><hr><br />
@@ -57,8 +142,9 @@
          <form action="shoppingCart.php">
            <?=displayClothes()?>  
            <br />
+           
            <input type="submit" value="Continue">
          </form>  
+         
         
     </body>
-</html>
